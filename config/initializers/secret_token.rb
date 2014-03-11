66477ea96dbc5f2f40583342b4fb1a26008116d6
xiaofen <oulafen@gmail.com>
2014-03-11 09:50:46 +0800

    Init the accupass_fb

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AccupassFb::Application.config.secret_key_base = '09f075ab4933ca453c382a450faa525f8c3e2dd0f1b07b66a339ca8a7a7c18d4070c73e9aae5f8ca7141645caa6e892da6fb9e3a378498bc21651247ae684319'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AccupassFb::Application.config.session_store :cookie_store, key: '_accupass-fb_session'
