0168b45ec62bc30c335b2de3bc6e203a03bcde2c
xiaofen <oulafen@gmail.com>
2014-03-14 21:22:37 +0800

    Generate the model and table about admin

--- db/migrate/20140311021030_create_users.rb
+++ db/migrate/20140311021030_create_users.rb
@@ -5,13 +5,9 @@ class CreateUsers < ActiveRecord::Migration
       t.string :password_digest
       t.string :forgot_pw_question
       t.string :forgot_pw_answer
-      #t.string :token
 
       t.timestamps
     end
-    #add_column :users, :token, :string
-
-
-
+     
   end
 end
